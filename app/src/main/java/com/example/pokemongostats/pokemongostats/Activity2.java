8bbd3766ec60fd98fdebd1da87adb17cad21da12
alvarosuafe <alvarosuafe@gmail.com>
2018-12-12 02:39:55 +0100

    Añadida actividad2 para mostrar un pokemon en detalle

---
+++ app/src/main/java/com/example/pokemongostats/pokemongostats/Activity2.java
@@ -0,0 +1,14 @@
+package com.example.pokemongostats.pokemongostats;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Activity2 extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detailview_pokemon);
+
+    }
+
+}
